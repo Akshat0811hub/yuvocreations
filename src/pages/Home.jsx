4cd0b193e7eb10a2d30cad31c84f783669371fb9
Akshat0811 <singhakshat875@gmail.com>
2025-09-29 23:18:11 +0530

    banner changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,19 @@
 import React from 'react' 
-import ParallaxMouseEffect from '../components/ParallaxMouseEffect'
+// import ParallaxMouseEffect from '../components/ParallaxMouseEffect'
 import Tabs from '../components/Tabs'
 import Contact from '../components/Contact'
 // import About from '../components/About'
 import ImgCarousel from '../components/ImgCarousel'
 import PremiumBezierCurve from '../components/beizercurve'
+import FluidCanvas from '../components/FluidCanvas'
 
 
 const Home = () => {
   return (
     <div className='hero-section'>
+      <FluidCanvas/>
       {/* <Header/> */}
-      <ParallaxMouseEffect/>
+      {/* <ParallaxMouseEffect/> */}
       <Tabs/>
       {/* <About/> */}
       <PremiumBezierCurve/>
